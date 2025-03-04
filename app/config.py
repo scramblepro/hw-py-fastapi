5aab35bff22d05afa5e584a122899eb490b01edd
Dre <zaderjkazp@gmail.com>
2025-03-04 22:19:59 +0300

    adapted and workfull

---
+++ app/config.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+POSTGRES_DB = os.getenv("POSTGRES_DB", "rest_na_fast")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
+POSTGRES_USER = os.getenv("POSTGRES_USER", "KiryhaTheBest")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "secret")
+
+PG_DSN = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+
+TOKEN_TTL = int(os.getenv("TOKEN_TTL", 60))
+
+SQL_DEBUG = os.getenv("SQL_DEBUG", "False").lower() in ("true", "1")
+DEFAULT_ROLE = os.getenv("DEFAULT_ROLE", "user")
